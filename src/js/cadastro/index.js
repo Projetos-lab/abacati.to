ca70aa5a0c17e2ae3716c085ebbf5ff01bc07808
jefferson.matos <jefferson.matos@anchieta.br>
2021-09-02 14:13:43 -0300

    add icon and flat icon

--- src/js/cadastro/index.js
+++ src/js/cadastro/index.js
@@ -4,6 +4,25 @@ const addRow = () => {
   var weight = $("#weight").val();
   var height = $("#height").val();
   var IMC = parseFloat(weight / (height * height)).toFixed(2);
+  var status = "nada";
+  if (IMC < 18.5 && age < 60) {
+    status = "Baixo";
+  } else if (IMC >= 18.5 && IMC < 25 && age < 60) {
+    status = "Adequado";
+  } else if (IMC >= 25 && IMC <= 29.9 && age < 60) {
+    status = "Sobrepeso";
+  } else if (IMC >= 30 && age < 60) {
+    status = "Obesidade";
+  } else if (IMC < 21.9 && age > 60) {
+    status = "Baixo";
+  } else if (IMC > 27 && age > 60) {
+    status = "Sobrepeso";
+  } else{
+    status= "Adequado";
+  }
+
+
+
   var markup =
     "<tr><td><input type='checkbox' name='record'></td><td>" +
     name +
@@ -15,6 +34,8 @@ const addRow = () => {
     height +
     "</td><td>" +
     IMC +
+    "</td><td>" +
+    status +
     "</td></tr>";
 
   $("table tbody").append(markup);
