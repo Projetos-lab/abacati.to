e89b0552ddaeedbf89e460690de4b87b57724eb9
Camila <65414346+cahmilex@users.noreply.github.com>
2021-09-02 10:17:50 -0300

    Add files via upload

---
+++ src/js/cadastro/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".add-row").click(function(){
+        var name = $("#name").val();
+        var age = $("#age").val();
+        var weight = $("#weight").val();
+        var height = $("#height").val();
+        var IMC = parseFloat(weight/(height*height)).toFixed(2);
+        var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + age + "</td><td>" + weight + "</td><td>" + height + "</td><td>" + IMC + "</td></tr>";
+
+        $("table tbody").append(markup);
+    });
+    
+    // Find and remove selected table rows
+    $(".delete-row").click(function(){
+        $("table tbody").find('input[name="record"]').each(function(){
+            if($(this).is(":checked")){
+                $(this).parents("tr").remove();
+            }
+        });
+    });
+});    
+
+function trocaCor(){
+    changeColorHeader("#F3DE2C");
+}
